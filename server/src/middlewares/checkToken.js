a74e2f429ece35b893d83860dd871384e3f9ce31
Felipe Buso <felipe_buso@hotmail.com>
2021-11-03 07:53:49 -0300

    refactor: improved token authentication

--- server/src/middlewares/checkToken.js
+++ server/src/middlewares/checkToken.js
@@ -1,13 +1,19 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+const UserModel = require('../models/userModel');
+
 const secret = process.env.TOKEN_SECRET;
 
 const validateToken = async (req, res, next) => {
   const token = req.headers.authorization;
 
   try {
-    jwt.verify(token, secret);
+    const decoded = jwt.verify(token, secret);
+    const user = await UserModel.findUserByEmail(decoded.userEmail);
+    if (user === null) {
+      return res.status(404).json({ message: 'Usuário não cadastrado.' });
+    }
     next();
   } catch (error) {
     return res.status(401).json({ message: 'Token inválido' });
